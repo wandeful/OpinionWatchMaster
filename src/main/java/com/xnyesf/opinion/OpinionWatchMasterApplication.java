dc251861457b70c340ad2fc1e1fa894e909ba4be
CAO <jsu_clb@163.com>
2024-11-29 18:44:02 +0800

    maven初始化

---
+++ src/main/java/com/xnyesf/opinion/OpinionWatchMasterApplication.java
@@ -0,0 +1,15 @@
+package com.xnyesf.opinion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class OpinionWatchMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpinionWatchMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xnyesf/opinion/OpinionWatchMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xnyesf.opinion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinionWatchMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
